6a6579e6c9f94d83cb6066234048546fb4d5f75a
hp <hp@DESKTOP-J13SQ09>
2023-07-21 09:37:11 +0100

    start about section

---
+++ resources/views/aboutPages/home.blade.php
@@ -0,0 +1,28 @@
+<div>
+    <div class="container">
+        <div class="left-about">
+            <img src="{{src('/images/about-image.jpg')}}"/>
+        </div>
+        <div class="right-about">
+            <div class="about-header">
+                <span class="sous-titre">Explore-Now</span>
+                <h1 class="titre-about">We Offer Top Listings For The City</h1>
+                <p class="p-about">Kulla quis velit vel nunc commodo ullamcorper. Pellentesque tincidunt et nisl eget porta. Vestibulum vel viverra sapien. Nullam a iaculis purus. Duis aliquam lorem at arcu iaculis venenatis.</p>
+            </div>
+            <div class="check-items">
+                <div class="right-items">
+                    <h3>100% genuine & verified places</h3>
+                </div>
+                <div class="left-items">
+                    
+                </div>
+            </div>
+            <div class="satisfaction">
+
+            </div>
+            <div class="btn-view-more">
+
+            </div>
+        </div>
+    </div>
+</div>
