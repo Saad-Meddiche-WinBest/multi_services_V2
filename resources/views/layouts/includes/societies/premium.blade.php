70deb1449fb0168e0545425b13af65d5a3f1f1dc
hp <hp@DESKTOP-J13SQ09>
2023-07-21 16:02:58 +0100

    Modif premium

--- resources/views/layouts/includes/societies/premium.blade.php
+++ resources/views/layouts/includes/societies/premium.blade.php
@@ -1,38 +1,12 @@
-<section class="elementor-section elementor-top-section elementor-element elementor-element-ccb6e48 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="ccb6e48" data-element_type="section" data-settings="{&quot;wdt_bg_image&quot;:{&quot;url&quot;:&quot;&quot;,&quot;id&quot;:&quot;&quot;,&quot;size&quot;:&quot;&quot;},&quot;wdt_bg_image_widescreen&quot;:{&quot;url&quot;:&quot;&quot;,&quot;id&quot;:&quot;&quot;,&quot;size&quot;:&quot;&quot;},&quot;wdt_bg_image_laptop&quot;:{&quot;url&quot;:&quot;&quot;,&quot;id&quot;:&quot;&quot;,&quot;size&quot;:&quot;&quot;},&quot;wdt_bg_image_tablet_extra&quot;:{&quot;url&quot;:&quot;&quot;,&quot;id&quot;:&quot;&quot;,&quot;size&quot;:&quot;&quot;},&quot;wdt_bg_image_tablet&quot;:{&quot;url&quot;:&quot;&quot;,&quot;id&quot;:&quot;&quot;,&quot;size&quot;:&quot;&quot;},&quot;wdt_bg_image_mobile_extra&quot;:{&quot;url&quot;:&quot;&quot;,&quot;id&quot;:&quot;&quot;,&quot;size&quot;:&quot;&quot;},&quot;wdt_bg_image_mobile&quot;:{&quot;url&quot;:&quot;&quot;,&quot;id&quot;:&quot;&quot;,&quot;size&quot;:&quot;&quot;},&quot;wdt_bg_position&quot;:&quot;center center&quot;,&quot;wdt_animation_effect&quot;:&quot;none&quot;}">
-    <div class="elementor-container elementor-column-gap-no">
-        <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-381ec4c" data-id="381ec4c" data-element_type="column">
-            <div class="elementor-widget-wrap elementor-element-populated">
-                <div class="elementor-element elementor-element-c7ee5c6 start wdt-custom-heading-home-2 elementor-widget__width-initial elementor-widget elementor-widget-wdt-heading animated fadeInLeft" data-id="c7ee5c6" data-element_type="widget" data-settings="{&quot;separator_background_background&quot;:&quot;classic&quot;,&quot;separator_background_color&quot;:&quot;#EB395100&quot;,&quot;_animation&quot;:&quot;fadeInLeft&quot;,&quot;wdt_animation_effect&quot;:&quot;none&quot;}" data-widget_type="wdt-heading.default">
-                    <div class="elementor-widget-container">
-                        <div class="wdt-heading-holder" id="wdt-heading-c7ee5c6">
-                            <div class="wdt-heading-subtitle-wrapper wdt-heading-align-center">
-                                <span class="wdt-heading-subtitle">Popular Events</span>
-                            </div>
-                            <div class="wdt-heading-separator-wrapper">
-                                <div class="wdt-heading-separator with-line">
-                                    <div class="wdt-separator-line">
-                                    </div>
-                                    </div>
-                                </div>
-                                <h2 class="wdt-heading-title-wrapper wdt-heading-align-center wdt-heading-deco-wrapper">
-                                    <span class="wdt-heading-title">
-                                        Enjoy the happenings
-                                    </span>
-                                </h2>
-                                <div class="wdt-heading-content-wrapper">
-                                    Volutpat consequat mauris nunc congue nisi. Sed ullamcorper morbi tincidunt ornare.
-                                     Ac placerat ves lectus
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-<div class="">
-        <div class="">
-            
-        </div>		
-</div>
-</div>
-</div>
-</div>
-        </div>
-</section>
+<div class="slider-container">
+    <div class="slider">
+      <div class="cards">
+        <!-- Card 1 content -->
+      </div>
+      <div class="cards">
+        <!-- Card 2 content -->
+      </div>
+      <!-- Add more cards as needed -->
+    </div>
+  </div>
+  
