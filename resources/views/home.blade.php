cd3af6b7cc6c5c857fb04aeaae874a55b9e0cf7d
hp <hp@DESKTOP-J13SQ09>
2023-07-24 11:23:37 +0100

    reform structure home/navbar v1 done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     @include('layouts.includes.user.searchbar')
     @include('layouts.includes.societies.premium')
-    @include('layouts.includes.cities.index')
     @include('layouts.includes.aboutPages.index')
+    @include('layouts.includes.cities.index')
     @include('layouts.includes.categories.index')
     @include('layouts.includes.societies.index')
 @endsection
--- resources/views/layouts/includes/user/navbar.blade.php
+++ resources/views/layouts/includes/user/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-md navbar-light bg-black shadow-sm">
+<nav class="navbar navbar-expand-md navbar-light bg-black shadow-sm" style="background-color: rgb(22, 22, 22);">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <a class="navbar-brand" style="color: white" href="{{ url('/') }}">
             <b>Multi</b>Serivces
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -13,19 +13,26 @@
             <ul class="navbar-nav me-auto">
 
             </ul>
-
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ms-auto">
                 <!-- Authentication Links -->
-
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                    <button class="btn-devis">
+                        <a class="nav-link" style="color:black;padding:0 5px" href="{{ route('home') }}">
+                            
+                            <img src="{{asset('assets/images/new_icon.png')}}" style="position: relative;left: 0;z-index: 999;width: 40px;height: fit-content;bottom: 1px;" />
+                            {{-- <i class="fa fa-bullhorn"></i> --}}
+                            {{ __('Demande de devis') }}</a>
+                    </button>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('societies.index') }}">{{ __('Societies') }}</a>
+                    <a class="nav-link" style="color:white;text-decoration:underline;" href="{{ route('societies.index') }}">{{ __('Découvrir nos solutions') }}</a>
                 </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item">
+                    <a class="nav-link" style="color:white;text-decoration:underline;" href="{{ route('societies.index') }}">{{ __('Inscription Etablissement') }}</a>
+                </li>
+                {{-- <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ __('Pages') }}
@@ -42,7 +49,7 @@
                             {{ __('Page 3') }}
                         </a>
                     </div>
-                </li>
+                </li> --}}
             </ul>
         </div>
     </div>
