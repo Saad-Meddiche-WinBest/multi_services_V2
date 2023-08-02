34eadf5b3904177cddd1a0c900a706c87cb806ac
S-A-3-D-O-U-N-E <112890463+saadMeddiche@users.noreply.github.com>
2023-08-02 12:20:15 +0100

    change the name of files in layouts.includes.societies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,9 @@
 
 @section('content')
     @include('layouts.includes.user.searchbar')
-
-    @include('layouts.includes.societies.premium')
+    @include('layouts.includes.societies.premiumSocieties')
     @include('layouts.includes.aboutPages.index')
     @include('layouts.includes.cities.index')
     @include('layouts.includes.categories.index')
-    @include('layouts.includes.societies.index')
-
-
+    @include('layouts.includes.societies.newSocieties')
 @endsection
