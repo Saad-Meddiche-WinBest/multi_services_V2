2b8e467a2b1b11b1acdfd102eae0ee4e3fb8cf1d
S-A-3-D-O-U-N-E <112890463+saadMeddiche@users.noreply.github.com>
2023-08-03 14:13:53 +0100

    Add moroccan cities

--- database/seeders/CitiesTableSeeder.php
+++ database/seeders/CitiesTableSeeder.php
@@ -18,10 +18,23 @@ class CitiesTableSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        foreach (range(1, 30) as $index) {
+        $cities = [
+            'Casablanca', 'Rabat', 'Fes', 'Marrakech', 'Tangier', 'Meknes', 'Agadir', 'Oujda', 'Kenitra', 'Tetouan',
+            'Safi', 'Mohammedia', 'Khouribga', 'Beni Mellal', 'Fes al Bali', 'El Jadida', 'Taza', 'Nador', 'Settat', 'Larache',
+            'Ksar El Kebir', 'Guelmim', 'Berrechid', 'Oued Zem', 'Taourirt', 'Berkane', 'Sidi Slimane', 'Sidi Qacem', 'Khemisset',
+            'Taroudant', 'Essaouira', 'Tiflet', 'Oulad Teima', 'Sefrou', 'Youssoufia', 'Tiznit', 'Lqliaa', 'Jerada', 'Khenifra',
+            'Ben Guerir', 'Ouarzazate', 'Ain Harrouda', 'Skhirate', 'Tirhanimine', 'Tinghir',
+            'Chefchaouene', 'Dakhla', 'Ifrane', 'Bouznika', 'Ouazzane',
+            'Tahala', 'Zagora', 'Taounate', 'Sidi Bennour', 'Sidi Bennour', 'Azrou', 'Tan-Tan', 'Ouezzane', 'Mdiq', 'El Aioun',
+            'Zaio', 'Tiflet', 'Zawyat an Nwaçer', 'Boujniba', 'Guercif', 'Imzouren', 'Fnideq', 'Zeghanghane', 'Asilah', 'Ait Melloul',
+            'Lakhsas', 'Souq Khamis', 'Ahfir', 'Al Hoceima', 'El Hajeb', 'Tetouan', 'Ain Cheggag', 'Midelt', 'Azemmour', 'Larache',
+            'Aghbala'
+        ];
+
+        foreach ($cities as $city) {
             DB::table('cities')->insert([
-                'name' => $faker->city,
-                'image' => $faker->imageUrl(),      
+                'name' => $city,
+                'image' => $faker->imageUrl(),
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
