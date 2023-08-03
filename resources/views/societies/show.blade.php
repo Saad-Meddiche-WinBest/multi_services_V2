59480247d860ec27a7f96d56681a8a6d251e44be
hp <hp@DESKTOP-J13SQ09>
2023-08-03 12:55:25 +0100

    fix conflit after merge

--- resources/views/societies/show.blade.php
+++ resources/views/societies/show.blade.php
@@ -502,47 +502,7 @@
                         @endforeach
                     </div>
                     @endif
-                    {{-- Extra Info --}}
-                    <div class="description-details">
-                        <div class="description-title-extra">
-                            <h1>Listing Info</h1>
-                        </div>
-                        <div class="tr-single-body">
-                            <ul class="extra-service">
-                                <li>
-                                    <div class="icon-box-icon-block">
-                                        <a href="https://www.google.com/maps/search/?api=1&amp;query=24.264129770801087,90.1659532603068"
-                                            target="_blank">
-                                            <div class="icon-box-round">
-                                                <svg xmlns="http://www.w3.org/2000/svg" height="1em"
-                                                    viewBox="0 0 384 512">
-                                                    <path
-                                                        d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
-                                                </svg>
-                                            </div>
-                                            <div class="icon-box-text">
-                                                {{ $societie->adress }}
-
-                                            </div>
-                                    </div>
-                                    <div class="description-title-right">
-                                        <span style="color:#df1010">Closed</span>
-                                    </div>
-                                @else
-                                    <div class="society-status">
-                                        <span><b>{{ $schedule->day->day }}</b></span>
-                                    </div>
-                                </div>
-                                <div class="description-title-right">
-                                    <span>{{ $schedule->from }} | {{ $schedule->until }}</span>
-                                </div>
-                        @endif
-
-                    </div>
-
-                </div>
-                @endforeach
-            </div>
+                    
 
             {{-- Extra Info --}}
             <div class="description-details">
